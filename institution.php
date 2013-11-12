63c6e7f5244503d29620b6d613c63fa21e47633b
arecaps <arecaplan@gmail.com>
2013-11-12 17:26:47 -0500

    combined

--- institution.php
+++ institution.php
@@ -11,8 +11,11 @@ try {
 echo $e->getMessage();
 }
 
-$DBH->query('DROP DATABASE IF EXISTS institutions');
+//$DBH->query('DROP DATABASE IF EXISTS institutions');
 $DBH->query('CREATE DATABASE institutions');
+
+//$DBH->query('DROP TABLE IF EXISTS institutions');
+$DBH->query('CREATE TABLE institutions.institutions (ID INT( 6 ) NOT NULL , Name VARCHAR( 120 ) NOT NULL , Address VARCHAR( 100 ) NOT NULL , City VARCHAR( 30 ) NOT NULL , State VARCHAR( 2 ) NOT NULL , Zip INT ( 10 ) NOT NULL , Website VARCHAR( 100 ) NOT NULL , Highest_Degree_Offered VARCHAR( 100 ) NOT NULL , Latitude DECIMAL( 10, 6 ) NOT NULL , Longitude DECIMAL( 10, 6 ) NOT NULL)');
 
 $DBH = null;
 ?>
