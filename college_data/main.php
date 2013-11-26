0cad50ac597962cfa40a976f167a425b56c8c95e
arecaps <arecaplan@gmail.com>
2013-11-25 20:25:30 -0500

    The php code to display queries as a table

--- college_data/main.php
+++ college_data/main.php
@@ -7,21 +7,7 @@ include_once('pdo_connect.php');
 			ORDER BY Enrollment DESC LIMIT 10;");
 	$STH->setFetchMode(PDO::FETCH_ASSOC);
 	$STH->execute();
-		$data = $STH->fetchall();
-                print_r($data);			
-  	echo "<table border=1><thead><tr>";
-  			
-            foreach($data[0] as $key=> $value){
-  		echo "<th>" . $key . "</th>";
-            }
-  			
-        echo "</tr></thead><tbody>";
-            foreach ($data as $outer =>$inner) {  
-                echo '<tr>';
-                   foreach($inner as $key => $value){
-                      echo "<th>" . $value . "</th>";  
-                   }
-                 echo '</tr>';  
-            }
-	echo "</tbody></table></body>";
+		$data = $STH->fetchall();		
+  
+               include_once ('table.php');
 ?>
