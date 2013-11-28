7f0bbb9b624eb86d23a55f74ff6958ac720b61ab
arecaps <arecaplan@gmail.com>
2013-11-28 16:15:52 -0500

    writes the results to a table

--- college_data/table.php
+++ college_data/table.php
@@ -1,28 +1,32 @@
 <!DOCTYPE html>
 <html>
-    <head>
+    <head><title></title>
+        <link rel="stylesheet" type="text/css" href="college.css">
     </head>
     <body>
-        <table border=1>
-            <thead>
-                 <tr>
+        <h1>Your #1 Source for College Data!</h1>
+        <div class ="centered">
+            <table border=1>
+                <thead>
+                     <tr>
+                    <?php
+                    foreach($data[0] as $key=> $value){
+                        echo "<th>" . $key . "</th>";
+                    }?>
+                     </tr>
+                </thead>
+                <tbody>
                 <?php
-                foreach($data[0] as $key=> $value){
-                    echo "<th>" . $key . "</th>";
-                }?>
-                 </tr>
-            </thead>
-            <tbody>
-            <?php
-                foreach ($data as $outer =>$inner) {  
-                    echo '<tr>';
-                    foreach($inner as $key => $value){
-                      echo "<th>" . $value . "</th>";  
-                    }
-                 echo '</tr>';  
-                }?>
-             </tbody>
-        </table>
+                    foreach ($data as $outer =>$inner) {  
+                        echo '<tr>';
+                        foreach($inner as $key => $value){
+                          echo "<td>" . $value . "</td>";  
+                        }
+                     echo '</tr>';  
+                    }?>
+                 </tbody>
+            </table>
+        </div>
     </body>
 </html>
      
