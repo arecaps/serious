f4a59e7539bf1b1d74f849f535ddff3d566d0495
arecaps <arecaplan@gmail.com>
2013-12-15 19:11:52 -0500

    compare while and for

---
+++ whileFor.java
@@ -0,0 +1,18 @@
+public class moreExamp {
+    public static void main(String[] args){
+        int sum = 0;
+            for(int j=0; j<5; j++){
+                sum += j;
+            }
+                System.out.println(sum);
+
+    
+    int i = 1;
+    int total = 0;
+        while(i < 5){
+            total = total + i;
+            i++;
+        }
+                System.out.println(total);        
+    }
+}
