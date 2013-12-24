f9492c1dd25888616fd705c346e246151e0abca0
arecaps <arecaplan@gmail.com>
2013-12-23 20:55:41 -0500

    Works as it should

--- gradeBook.java
+++ gradeBook.java
@@ -38,8 +38,25 @@ public class GradeBook {
                             more = sc.next();
                         }
             }
-            for(int j=0; j< students.size(); j++)
+            for(int j=0; j< students.size(); j++){
                 System.out.println(students.get(j).getName() + ": " + students.get(j).getAverage());
-    } 
+            }
+     
+     String choice2 = "y";
+     while(choice2.equalsIgnoreCase("y")){       
+     System.out.print("Enter a name to get the average for that student: ");
+     String find = sc.next();
+        for (Student s : students) {
+            if (s.getName().equals(find)){
+                System.out.println(s.getName() + ": " + s.getAverage());
+                System.out.print("Would you like to get the average for another student? (y/n): ");
+                choice2 = sc.next();
+                    if(!choice2.equalsIgnoreCase("y") & !choice2.equalsIgnoreCase("n")){
+                        System.out.print("Please enter either \"y\" or \"n\": ");
+                        choice2 = sc.next();
+                    }
+            }
+        }
+    }
+    }
 }
-
